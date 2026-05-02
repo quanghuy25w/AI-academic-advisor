1dac52a50ebe96e1a00e242c43f684908a0def32
QuangHuy <huynguyen1622005@gmail.com>
2026-05-02 14:03:25 +0700

    fix

--- src/config/email_config.py
+++ src/config/email_config.py
@@ -4,10 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class EmailConfig:
-<<<<<<< HEAD
-    SENDER_EMAIL = "dinhtruongphong2752004@gmail.com"
-    APP_PASSWORD = "hxhi ijay qlav gpxo"
-=======
     SENDER_EMAIL = os.getenv("SENDER_EMAIL", "")
     APP_PASSWORD = os.getenv("APP_PASSWORD", "")
->>>>>>> temp-branch
